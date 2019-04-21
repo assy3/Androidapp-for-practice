89b6b74d8b8d9ad604e1841039ff64e261cea3f5
Takumi Ashikawa <b1748628@planet.kanazawa-it.ac.jp>
2019-04-22 03:54:19 +0900

    リストビュースピナーを追加

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     Button bt2;
     RadioButton rb[] = new RadioButton[3];
     RadioGroup rg;
+    ListView lv1, lv2;
+    Spinner sp;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +50,33 @@ public class MainActivity extends AppCompatActivity {
         rb[0].setChecked(true);
 
 
+        lv1 = new ListView(this);  //リストビュー1の生成
+        lv2 = new ListView(this);  //リストビュー2の生成
+        sp = new Spinner(this);  //スピナーの生成
+
+        String[] str ={"兼六園","21世紀美術館","近江町市場",
+                "東茶屋街","武家屋敷","忍者寺"};  //リストビュー、スピナーに格納するテキストデータ
+
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, str);  //テキストをリストビュー、スピナーに渡すためのアレイアダプター
+        lv1.setAdapter(ad);  //リストビュー1にアレイアダプターを登録
+        lv2.setAdapter(ad);  //リストビュー2にアレイアダプターを登録
+
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);  //アレイアダプターにドロップダウンリストを設定
+        sp.setAdapter(ad);  //スピナーにアレイアダプターを登録
+
+        ScrollView sv = new ScrollView(this);  //スクロールビューの生成
+
+
         ll.addView(tv);
         ll.addView(bt);
         ll.addView(bt2);
         ll.addView(et);
         ll.addView(rg);  //リニアレイアウトへラジオグループの登録
+        ll.addView(lv1);  //リニアレイアウトにリストビューを設定
+        ll.addView(sp);  //リニアレイアウトにスピナーを設定
+        ll.addView(sv);  //リニアレイアウトにスピナーを設定
+
+        sv.addView(lv2);  //スクロールビューにリストビューを設定
 
         //匿名クラスによる記述法
         bt2.setOnClickListener(
@@ -69,12 +93,31 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i<rb.length; i++)
             rb[i].setOnClickListener(new ExSampleClickListener());  //ラジオボタンクリック時のリスナー登録
 
+        lv1.setOnItemClickListener(new ExSampleItemClickListener());  //リストビュー1のアイテムクリック時のリスナー登録
+        lv2.setOnItemClickListener(new ExSampleItemClickListener());  //リストビュー2のアイテムクリック時のリスナー登録
+        sp.setOnItemSelectedListener(new ExSampleItemSelectedListener());  //スピナーのアイテム選択時のリスナー登録
     }
 
     class ExSampleClickListener implements OnClickListener{  //ボタンがクリックされたときのイベント処理
         public void onClick(View v){
             tv.setText("兼六園がおすすめです！");
         }  //ボタンがクリックされたときの具体的な処理
+    }
+
+
+    class ExSampleItemClickListener implements AdapterView.OnItemClickListener {  //リストビューのアイテムクリック時のイベント処理
+        public void onItemClick(AdapterView<?>v, View iv, int pos, long id){
+            TextView tmp = (TextView) iv;
+            tv.setText(tmp.getText() + "を表示します。");
+        }
+    }
+
+    class ExSampleItemSelectedListener implements AdapterView.OnItemSelectedListener {  //スピナーのアイテムクリック時のイベント処理
+        public void onItemSelected(AdapterView<?>v, View iv, int pos, long id){
+            TextView tmp = (TextView) iv;
+            tv.setText(tmp.getText() + "を表示します。");
+        }
+        public void onNothingSelected(AdapterView<?> arg0){}
     }
 
 /*    class ExSampleTouchListener implements OnTouchListener{  //ボタンがタッチされたときのイベント処理
